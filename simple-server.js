e7c0cb2049b01d2a63195d470410e2f4f8cccb84
Steven <sgm4689@MSS3110-11>
2020-02-18 14:50:39 -0500

    Finished server

--- simple-server.js
+++ simple-server.js
@@ -16,6 +16,56 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 let jokesArray;
 
 // request handlers
+const handlerNotFound = (request,response) => {
+    const statusCode = 404;
+    response.writeHead(statusCode, {'content-type': 'text/html'});
+    response.write('<html><body><h1>404 - Not Found!</h1></body></html>');//I is gud programmar
+    response.end();
+}
+
+const handlerGetAllJokes = (request, response) => {
+    const statusCode = 200;
+    response.writeHead(statusCode, {'content-type': 'application/json'});
+    response.write(JSON.stringify(jokesArray));//I is gud programmar
+    response.end();
+}
+
+const handlerGetRandomJokes = (request, response) => {
+    const statusCode = 200;
+    response.writeHead(statusCode, {'content-type': 'application/json'});
+    response.write(JSON.stringify(jokesArray[Math.floor(Math.random() * jokesArray.length)]));//I is gud programmar
+    response.end();
+}
+
+
+const urlStruct = {
+  
+    'GET' : {
+        '/getAllJokes':handlerGetAllJokes,
+        '/getRandomJokes':handlerGetRandomJokes
+    },
+    'POST' : {},
+    'Head' : {},
+    notFound: handlerNotFound
+    
+};
+
+const onRequest = (request, response) => {
+    const parsedUrl = url.parse(request.url);
+    const pathname = parsedUrl.pathname;
+    const httpMethod = request.method;
+    const params = query.parse(parsedUrl.query);
+    
+    console.log(`pathname = ${pathname}`);
+    console.log(`httpMethod = ${httpMethod}`);
+    console.log(`params = ${Object.keys(params)}`);
+    
+    if (urlStruct[httpMethod][pathname])
+        urlStruct[httpMethod][pathname](request,response);
+        
+        else
+            urlStruct.notFound(request,response);
+}
 
 // urlStruct
 
@@ -24,11 +74,13 @@ let jokesArray;
 
 let init = () => {
 	// load jokes
-	const jokes = fs.readFileSync(`${__dirname}/jokes.json`);
-	
+	const jokeFile = fs.readFileSync(`${__dirname}/jokes.json`);
+    jokesArray = JSON.parse(jokeFile).jokes;
+    console.log(`There are ${jokesArray.length} jokes`);
+    
 	// create server
 	http.createServer(onRequest).listen(port);
 	console.log(`Listening on 127.0.0.1: ${port}`);
 }
 
-//init();
+init();
